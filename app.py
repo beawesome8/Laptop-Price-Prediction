Updated Streamlit app and fixed resolution calculation

app.py
@@ -1,9 +1,10 @@
 import streamlit as st
 import pickle
+import numpy as np
 
 #import the model
-pipe = pickle.load(open('pipe.pkl','rb'))
-laptops_df = pikcle.load(open('laptops_df.pkl','rb'))
+pipe = pickle.load(open('pipe_RF.pkl','rb'))
+laptops_df = pickle.load(open('laptops_df.pkl','rb'))
 
 st.title("laptop Price Predctor")
 
@@ -16,10 +17,10 @@ Model = st.selectbox('Model', laptops_df['Model'].unique())
 Series = st.selectbox('Series', laptops_df['Series'].unique())
 
 # RAM
-Ram = st.selectbox('Ram (GB)', laptops_df['Ram'], [2,4,6,8,12,16,24,32,64])
+Ram = st.selectbox('Ram (GB)', [2,4,6,8,12,16,24,32,64])
 
 # Weight
-Weight = st.number_input()
+Weight = st.number_input('Laptop Weight (kg)')
 
 # Touchscreen
 Touchscreen = st.selectbox('Touchscreen', ['No', 'Yes'])
@@ -28,13 +29,13 @@ Touchscreen = st.selectbox('Touchscreen', ['No', 'Yes'])
 IPS = st.selectbox('IPS', ['No', 'Yes'])
 
 # Screen Size
-Screen_Size = st.number_input('Screen Size')
+Screen_Size = st.number_input('Screen Size', min_value=1.0, value=15.6)
 
 # Resolution
-resolution = st.selectbox('Resolution',['1920x1080','1366x768','1600x900','3840x2160','3200x1800','2880x1800',
+Resolution = st.selectbox('Resolution',['1920x1080','1366x768','1600x900','3840x2160','3200x1800','2880x1800',
                                         '2560x1600','2560x1440','2304x1440'])
 #CPU
-cpu = st.selectbox('CPU',laptops_df['Cpu brand'].unique())
+Cpu = st.selectbox('CPU',laptops_df['Cpu brand'].unique())
 
 # HDD
 HDD = st.selectbox('HDD(in GB)',[0,128,256,512,1024,2048])
@@ -49,4 +50,22 @@ GPU = st.selectbox('GPU',laptops_df['Gpu Brand'].unique())
 OS = st.selectbox('OS',laptops_df['OS'].unique())
 
 if st.button('Predict Price'):
-    pass
+    # query
+    PPI = None
+    if Touchscreen == 'Yes':
+        Touchscreen = 1 
+    else:
+        Touchscreen = 0
+        
+    if IPS == 'Yes':
+        IPS = 1
+    else:
+        IPS = 0
+        
+    X_res = int(Resolution.split('x')[0])
+    y_res = int(Resolution.split('x')[1])
+    PPI = ((X_res**2) + (y_res**2))**0.5/Screen_Size
+    query = np.array([Model, Series, Ram, Weight, Touchscreen, IPS, PPI, Cpu, HDD, SSD, GPU, OS])
+    
+    query = query.reshape(1,12)
+    st.title(f'Predicted Price: {int(np.exp(pipe.predict(query)[0]))} Euros')